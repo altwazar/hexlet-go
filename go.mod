e4883cce83caa3e5dd0e48a6b86bd95d1c91825a
Denis Zhdanov <altwazar@gmail.com>
2025-07-07 15:56:14 +0700

    feat: changes for v2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/altwazar/hexlet-go
+module github.com/altwazar/hexlet-go/v2
 
 go 1.24.4
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"hexlet-go/greeting"
+	"github.com/altwazar/hexlet-go/v2/greeting"
 	// Здесь должен быть импорт пакета greeting
 )
 
